db31b5458d127042a9640e2db3e4525b9346f4fd
ilarsenal <30398407+alwayswithyou@users.noreply.github.com>
2025-05-29 21:15:05 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -5,13 +5,13 @@ radio.onReceivedNumber(function (receivedNumber) {
     basic.showString("" + (receivedNumber))
 })
 input.onButtonPressed(Button.A, function () {
-	
+    radio.sendString("play")
 })
 input.onButtonPressed(Button.AB, function () {
     radio.sendString("home")
 })
 input.onButtonPressed(Button.B, function () {
-	
+    radio.sendString("run")
 })
 radio.setGroup(1)
 basic.forever(function () {
